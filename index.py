3e81e0f4073cef8ca99a4dad4c87f55d9616448e
electricdinosaurs <notkarenyi@gmail.com>
2023-11-26 13:22:24 -0600

    add papers to streamlit

--- index.py
+++ index.py
@@ -8,17 +8,42 @@ Run a streamlit app.
 
 import pandas as pd
 import streamlit as st
+import string
+from nltk.tokenize import word_tokenize
+import re
+
+#%% import data
+
+df = pd.read_json('core-tutors-clean-2023-11-26.json')
+
+#%%
+
+def remove_punc(x):
+    return re.sub('\\'.join(string.punctuation),'',x)
 
 #%% streamlit app
 
 st.markdown('# Living literature review')
 
-st.sidebar.markdown('[Effective ways to increase high-school student achievement](#effective-ways-to-increase-high-school-student-achievement)')
+title = 'Effectiveness of tutoring'
+
+# sidebar section
+
+st.sidebar.markdown(f'[{title}](#{"-".join(title.lower().split(" "))})')
+
+for i,row in df.iterrows():
+    st.sidebar.markdown(f'[{row["title"]}](#{"-".join(remove_punc(row["title"]).lower().split(" "))})')
+
+# main section
 
 main, extra = st.columns([.7,.3])
 
-main.markdown('## Effective ways to increase high-school student achievement')
+main.markdown(f'## {title}')
 
 main.markdown('\n\n\n\n\n\n\n\n\n\nsdfsdf')
+
+for i,row in df.iterrows():
+    main.markdown(f'### {row["title"]}')
+    main.markdown(f'{row["methods"][:1000]}')
 
 extra.markdown('Here is extra content')
